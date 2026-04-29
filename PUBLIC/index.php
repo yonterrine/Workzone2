088ebe97084e3bd6bd422b45917ce7e33e968f36
yonterrine <iamyumigraysilvan@gmail.com>
2026-04-29 11:46:40 +0700

    first push

--- PUBLIC/index.php
+++ PUBLIC/index.php
@@ -1,17 +1,28 @@
-include
+<?php
 
-inquire
- inquire_once
-require
- require_once
+define('BASE_PATH', __DIR__);
 
+require_once basePath('helpers.php');
+require_once basePath('framework/router.php');
 
- <?php
- require '../VIEWERS/functions.php';
-define('BASE_PATH', dirname(__DIR__));
+function basePath(string $path = ''): string {
+    return rtrim(BASE_PATH, '/') . '/' . ltrim($path, '/');
+}
 
+// Re-require after basePath is available
+require_once __DIR__ . '/helpers.php';
 
-require basePath('helpers.php')
-LoadView('home');
+$router = new Router();
 
-require basePath('views/home.view.php');
+// Register routes
+$router->get('/',          'views/home.view.php');
+$router->get('/listings',  'views/home.view.php');
+
+// 403 demo route
+$router->get('/apply/restricted', function($params) {
+    http_response_code(403);
+    require basePath('controllers/listings/error.php');
+});
+
+// Dispatch current request
+$router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
